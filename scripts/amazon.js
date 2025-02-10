a7e38f72c636a1ad6a6d4e0b77163bf3d2b87494
affaxerd1 <affaxerdk@gmail.com>
2025-02-11 01:08:50 +0300

    check if product already exists and increase value or create a new product in array

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -64,10 +64,28 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
   button.addEventListener('click', ()=> {
     console.log(button.dataset.productName);
     const productName = button.dataset.productName
-    cart.push({
-      productName : productName,
-      quantity : 1
+    
+    
+
+    let matchingItem;
+    
+    cart.forEach((item) => {
+      if(productName === item.productName)
+        matchingItem = item
     })
+
+    if(matchingItem){
+      matchingItem.quantity += 1;
+
+    }
+    else {
+      cart.push({
+        productName : productName,
+        quantity : 1
+      })
+    }
+    console.log(cart);
+    
     
     
   })
