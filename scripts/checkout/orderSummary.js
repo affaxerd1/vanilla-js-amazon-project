7381d9aa297ea3d35f307e49d7f247025ad35950
affaxerd1 <affaxerdk@gmail.com>
2025-02-28 17:54:27 +0300

    update payment summary on delete

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -4,6 +4,7 @@ import {cart, removeItem, updateDeliveryOption} from '../../data/cart.js'
 import { formatCurrency } from "../utils/money.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions} from '../../data/deliveryOptions.js'
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 export function renderOrderSummary(){
 
@@ -84,6 +85,7 @@ export function renderOrderSummary(){
         const container = document.querySelector(`.js-cart-item-container-${productId}`);
         if (container) container.remove();
         
+        renderPaymentSummary()
 
       }
       )
@@ -115,6 +117,8 @@ export function renderOrderSummary(){
                   </div>
           `
         })
+
+        renderPaymentSummary()
 
       return deliveryOptionsaccum;
       }
